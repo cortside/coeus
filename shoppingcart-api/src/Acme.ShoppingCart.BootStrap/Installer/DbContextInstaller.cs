a62bea60599a1638be0147e97e58ea54ff5fae37
Cort Schaefer <cort@eschaefer.net>
2022-02-21 15:35:03 -0700

    add support for starting transaction in unit of work

--- shoppingcart-api/src/Acme.ShoppingCart.BootStrap/Installer/DbContextInstaller.cs
+++ shoppingcart-api/src/Acme.ShoppingCart.BootStrap/Installer/DbContextInstaller.cs
@@ -1,4 +1,3 @@
-using System;
 using Acme.ShoppingCart.Data;
 using Cortside.Common.BootStrap;
 using Microsoft.EntityFrameworkCore;
@@ -9,16 +8,16 @@ namespace Acme.ShoppingCart.BootStrap.Installer {
     public class DbContextInstaller : IInstaller {
         public void Install(IServiceCollection services, IConfigurationRoot configuration) {
             services.AddDbContext<DatabaseContext>(opt => {
-                opt.UseSqlServer(configuration.GetSection("Database").GetValue<string>("ConnectionString"),
-                    sqlServerOptionsAction: sqlOptions => {
-                        sqlOptions.EnableRetryOnFailure(
-                            maxRetryCount: 2,
-                            maxRetryDelay: TimeSpan.FromSeconds(1),
-                            errorNumbersToAdd: null);
-                    });
-                opt.EnableSensitiveDataLogging();
+                opt.UseSqlServer(configuration.GetSection("Database").GetValue<string>("ConnectionString")
+                    //,
+                    //sqlServerOptionsAction: sqlOptions => {
+                    //    sqlOptions.EnableRetryOnFailure(
+                    //        maxRetryCount: 2,
+                    //        maxRetryDelay: TimeSpan.FromSeconds(1),
+                    //        errorNumbersToAdd: null);
+                    //}
+                    );
             });
-            //services.AddScoped<DatabaseContext, DatabaseContext>();
 
             // Register the service and implementation for the database context
             services.AddScoped<IUnitOfWork>(provider => provider.GetService<DatabaseContext>());
--- shoppingcart-api/src/Acme.ShoppingCart.Data/IUnitOfWork.cs
+++ shoppingcart-api/src/Acme.ShoppingCart.Data/IUnitOfWork.cs
@@ -1,10 +1,14 @@
 using System;
+using System.Data;
 using System.Threading;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore.Storage;
 
 namespace Acme.ShoppingCart.Data {
     public interface IUnitOfWork : IDisposable {
         Task<int> SaveChangesAsync(CancellationToken cancellationToken = default(CancellationToken));
         Task<bool> SaveEntitiesAsync(CancellationToken cancellationToken = default(CancellationToken));
+
+        Task<IDbContextTransaction> BeginTransactionAsync(IsolationLevel isolationLevel);
     }
 }
