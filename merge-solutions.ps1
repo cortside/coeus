dd795fe9b041215af383a1453470584d0f530018
Cort Schaefer <cort@eschaefer.net>
2022-05-04 19:49:13 -0600

    update helper scripts

--- merge-solutions.ps1
+++ merge-solutions.ps1
@@ -1,4 +1,3 @@
-dotnet tool uninstall merge-solutions
 dotnet tool install --global merge-solutions
 
-merge-solutions /out .\coeus.sln .\shoppingcart-api\src\Acme.ShoppingCart.sln .\identityserver\src\Acme.IdentityServer.sln .\policyserver\src\PolicyServer.sln
+merge-solutions /out .\coeus.sln shoppingcart-api\src\Acme.ShoppingCart.sln identityserver\src\Acme.IdentityServer.sln policyserver\src\PolicyServer.sln
