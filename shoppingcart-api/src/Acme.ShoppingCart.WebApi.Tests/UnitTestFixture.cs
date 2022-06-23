248c0178a25c6ecba849dcb1a31fa49157a5e8f2
Cort Schaefer <cort@eschaefer.net>
2022-06-22 21:13:50 -0600

    [usecustomsubject] update to use cortside.aspnetcore libraries with changes to support service specific subject entity

--- shoppingcart-api/src/Acme.ShoppingCart.WebApi.Tests/UnitTestFixture.cs
+++ shoppingcart-api/src/Acme.ShoppingCart.WebApi.Tests/UnitTestFixture.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using Acme.ShoppingCart.Data;
+using Cortside.AspNetCore.Auditable;
 using Cortside.Common.Security;
 using Microsoft.AspNetCore.Http;
 using Microsoft.EntityFrameworkCore;
@@ -17,17 +18,17 @@ namespace Acme.ShoppingCart.WebApi.Tests {
 
         public Mock<T> Mock<T>() where T : class {
             var mock = new Mock<T>();
-            this.mocks.Add(mock);
+            mocks.Add(mock);
             return mock;
         }
 
         public DatabaseContext GetDatabaseContext() {
             var databaseContextOptions = new DbContextOptionsBuilder<DatabaseContext>().UseInMemoryDatabase($"db-{Guid.NewGuid():d}").Options;
-            return new DatabaseContext(databaseContextOptions, SubjectPrincipal.From(httpContextAccessorMock.Object.HttpContext.User));
+            return new DatabaseContext(databaseContextOptions, SubjectPrincipal.From(httpContextAccessorMock.Object.HttpContext.User), new DefaultSubjectFactory());
         }
 
         public void TearDown() {
-            this.mocks.ForEach(m => m.VerifyAll());
+            mocks.ForEach(m => m.VerifyAll());
         }
     }
 }
