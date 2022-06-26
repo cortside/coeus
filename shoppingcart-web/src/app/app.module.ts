0ab3feac50f31d0e5633c5f474da33d142498021
ruifang <ruifang.li09@gmail.com>
2022-06-26 17:59:13 -0600

    authentication

--- shoppingcart-web/src/app/app.module.ts
+++ shoppingcart-web/src/app/app.module.ts
@@ -1,29 +1,36 @@
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { AppConfig } from 'src/environments/app-config';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AuthModule } from './auth/auth.module';
+import { authenticationIntializerProvider } from './auth/authentication-initializer.provider';
+import { USER_MANAGER_SETTINGS_TOKEN } from './auth/user-manager-settings.token';
 import { ItemModule } from './item/item.module';
 import { OrderModule } from './order/order.module';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    HttpClientModule,
-    AuthModule,
+    declarations: [AppComponent],
+    imports: [
+        BrowserModule,
+        HttpClientModule,
 
-    // features
-    ItemModule,
+        // features
+        AuthModule,
+        ItemModule,
 
-    // route
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+        // route
+        AppRoutingModule,
+    ],
+    providers: [
+        // auth
+        { provide: USER_MANAGER_SETTINGS_TOKEN, useFactory: (config: AppConfig) => config.identity, deps: [AppConfig] },
+
+        // app init
+        authenticationIntializerProvider,
+    ],
+    bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
