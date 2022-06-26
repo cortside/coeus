4c1b8bc58699e75242cf257140319a81ab2f1e45
ruifang <ruifang.li09@gmail.com>
2022-06-26 17:59:13 -0600

    authentication

--- shoppingcart-web/src/app/auth/auth.module.ts
+++ shoppingcart-web/src/app/auth/auth.module.ts
@@ -1,18 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { UserManager } from 'oidc-client';
-import { userManagerFactory } from './user-manager.factory';
-import { USER_MANAGER_SETTINGS_TOKEN } from './user-manager-settings.token';
+import { AuthRoutingModule } from './auth-routing.module';
 
 @NgModule({
-    declarations: [],
-    imports: [CommonModule],
-    providers: [
-        {
-            provide: UserManager,
-            useFactory: userManagerFactory,
-            deps: [USER_MANAGER_SETTINGS_TOKEN],
-        },
-    ],
+    declarations: [
+  ],
+    imports: [CommonModule, AuthRoutingModule],
+    providers: [],
 })
 export class AuthModule {}
