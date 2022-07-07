c542aa33eb6dc2d02f04339e1651612e1395e842
ruifang <ruifang.li09@gmail.com>
2022-07-06 20:28:50 -0600

    fix auth routes

--- shoppingcart-web/src/app/auth/auth.module.ts
+++ shoppingcart-web/src/app/auth/auth.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AuthRoutingModule } from './auth-routing.module';
+import { EmptyComponent } from './empty/empty.component';
 
 @NgModule({
     declarations: [
+       EmptyComponent
   ],
     imports: [CommonModule, AuthRoutingModule],
     providers: [],
