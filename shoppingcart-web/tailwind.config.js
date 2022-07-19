b0b0eba6ae1b86be5d8845678b32828c5f3d6718
Ruifang Li <ruifang.li09@gmail.com>
2022-07-18 21:42:01 -0600

    add styles

--- shoppingcart-web/tailwind.config.js
+++ shoppingcart-web/tailwind.config.js
@@ -1,8 +1,39 @@
-/** @type {import('tailwindcss').Config} */
+const colors = require('tailwindcss/colors');
+
 module.exports = {
-  content: [],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+    content: ['./src/**/*.{html,ts}'],
+    darkMode: false, // or 'media' or 'class'
+    theme: {
+        fontFamily: {
+            sans: ['Roboto', 'ui-sans-serif', 'system-ui', '-apple-system', 'BlinkMacSystemFont', 'Helvetica Neue', 'Arial', 'Noto Sans', 'sans-serif'],
+        },
+        screens: {
+            sm: '576px',
+            md: '768px',
+            lg: '992px',
+            xl: '1200px',
+            '2xl': '1400px',
+        },
+        colors: {
+            transparent: 'transparent',
+            current: 'currentColor',
+            black: colors.black,
+            white: colors.white,
+            gray: colors.gray,
+            red: colors.red,
+            yellow: colors.yellow,
+            green: colors.green,
+            blue: colors.blue,
+            primary: colors.blue[700],
+            secondary: colors.gray[500],
+            success: colors.green[500],
+            warning: colors.yellow[700],
+            danger: colors.red[700],
+        },
+        extend: {},
+    },
+    variants: {
+        extend: {},
+    },
+    plugins: [require('@tailwindcss/forms')],
+};
