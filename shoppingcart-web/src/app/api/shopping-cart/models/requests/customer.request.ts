713933a6379c16d390473a61b2744c7c71e12c38
Ruifang Li <ruifang.li09@gmail.com>
2022-07-18 22:40:19 -0600

    add checkout

---
+++ shoppingcart-web/src/app/api/shopping-cart/models/requests/customer.request.ts
@@ -0,0 +1,10 @@
+export interface CustomerRequest {
+    firstName: string;
+    lastName: string;
+    email: string;
+    birthDate: {
+        year: number;
+        month: number;
+        day: number;
+    }
+}
--- shoppingcart-web/src/app/api/shopping-cart/shopping-cart.client.ts
+++ shoppingcart-web/src/app/api/shopping-cart/shopping-cart.client.ts
@@ -2,7 +2,9 @@ import { HttpClient, HttpContext } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { AUTHENTICATED_REQUEST } from '@muziehdesign/auth';
 import { Observable } from 'rxjs';
+import { OrderRequest } from './models/requests/order.request';
 import { AuthorizationResponse } from './models/responses/authorization.response';
+import { OrderReponse } from './models/responses/order.response';
 
 @Injectable({
     providedIn: 'root',
@@ -12,5 +14,9 @@ export class ShoppingCartClient {
 
     getAuthorization(): Observable<AuthorizationResponse> {
         return this.http.get<AuthorizationResponse>(`http://localhost:5000/api/v1/authorization`, { context: new HttpContext().set(AUTHENTICATED_REQUEST, true) });
+    }
+
+    createOrders(order: OrderRequest): Observable<OrderReponse> {
+        return this.http.post<OrderReponse>(`http://localhost:5000/api/v1/orders`, { context: new HttpContext().set(AUTHENTICATED_REQUEST, true) });
     }
 }
