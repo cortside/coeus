224151a3512e3b44bf6f18da917b28c89ce17b18
Ruifang Li <ruifang.li09@gmail.com>
2022-07-18 22:00:20 -0600

    add cart display

--- shoppingcart-web/src/app/core/shopping-cart.service.ts
+++ shoppingcart-web/src/app/core/shopping-cart.service.ts
@@ -1,9 +1,21 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { CartItemModel } from '../common/cart-item.model';
+import { ItemService } from './item.service';
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root',
 })
 export class ShoppingCartService {
+    private items = new BehaviorSubject<CartItemModel[]>([]);
+    private items$ = this.items.asObservable();
+    constructor() {}
 
-  constructor() { }
+    getItems(): Observable<CartItemModel[]> {
+      return this.items$;
+    }
+
+    addItem(itemId: string, quantity: number) {
+        this.items.next([...this.items.value, { itemId: itemId, quantity: quantity }]);
+    }
 }
