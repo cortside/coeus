85bbac4d052e8e2edbfb8d85066ae6848adca239
Ruifang Li <ruifang.li09@gmail.com>
2022-09-16 15:27:50 -0600

    correct packages

--- shoppingcart-web/src/app/app.module.ts
+++ shoppingcart-web/src/app/app.module.ts
@@ -2,7 +2,6 @@ import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { APP_INITIALIZER, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AuthenticationService, AuthenticationTokenInterceptor, AuthorizationService } from '@muziehdesign/auth';
-import { PageHeaderComponent } from 'muzieh-ngcomponents';
 import { ShoppingCartClient } from './api/shopping-cart/shopping-cart.client';
 import { initializeApplication } from './app-initializer';
 
@@ -19,7 +18,7 @@ import { NavigationComponent } from './navigation/navigation.component';
         BrowserModule,
         HttpClientModule,
         CoreModule,
-        
+
         ItemModule,
 
         // route
