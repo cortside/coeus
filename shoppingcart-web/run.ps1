b7f41a542c4fcf34b9d11d6cc63801e1d6e3a825
ruifang <ruifang.li09@gmail.com>
2023-03-29 14:52:02 -0600

    build then run in run script

--- shoppingcart-web/run.ps1
+++ shoppingcart-web/run.ps1
@@ -3,6 +3,6 @@ Param()
 
 Push-Location "$PSScriptRoot"
 
-cmd /c start cmd /k "title ShoppingCart Web - Angular & npm run start"
+cmd /c start cmd /k "title ShoppingCart Web - Angular & npm ci & npm run build & npm run start"
 
 Pop-Location
