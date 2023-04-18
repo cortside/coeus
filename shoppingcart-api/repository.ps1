23bfac3efec2167eda9b647d2e78cacfe6ea40f5
Cort Schaefer <cort@eschaefer.net>
2023-04-18 11:24:27 -0600

    [ARC-129] update to use module for generalized config; move more values to json so that there are not powershell scripts with values that vary be repo

--- shoppingcart-api/repository.ps1
+++
@@ -1,8 +0,0 @@
-# these values are referenced in scripts that use dotnet ef commands
-
-$repo = "Acme.ShoppingCart"
-$project = "src/$repo.Data"
-$startup = "src/$repo.WebApi"
-$context = "DatabaseContext"
-
-$repoConfig = if (Test-Path repository.json) { get-content repository.json | ConvertFrom-Json }
