00002f292f575acc17fad322fa275c82b525602c
Cort Schaefer <cort@eschaefer.net>
2023-05-08 16:23:20 -0600

    [ARC-133] add config.local.json for shoppingcart-web

--- shoppingcart-web/deploy/sh/startup.sh
+++ shoppingcart-web/deploy/sh/startup.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
 ifconfig
+if [[ -d /settings/$SERVICE_NAME ]]; then
+	cp -R /settings/$SERVICE_NAME/* /usr/share/nginx/html
+fi
+
+if [[ -f /app/init.sh ]]; then
+	/app/init.sh
+fi
 
 nginx -g "daemon off;"
