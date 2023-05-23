c1a82c079e6f417914b3eacb98bd7b84098b13ca
Cort Schaefer <cort@eschaefer.net>
2023-05-23 10:43:46 -0600

    cleanup commented out code

--- shoppingcart-api/update-template.ps1
+++ shoppingcart-api/update-template.ps1
@@ -6,8 +6,3 @@ dotnet new cortside-api-powershell --force --output ./ --name Acme.ShoppingCart 
 
 # update .editorconfig
 dotnet new cortside-api-editorconfig --force --output ./ --name Acme.ShoppingCart --company Acme --product ShoppingCart
-
-# update dockerfile and supporting shell and powershell scripts
-# if (Test-Path -path "deploy") {
-# 	dotnet new cortside-api-deploy --force --output ./ --name Acme.ShoppingCart --company Acme --product ShoppingCart
-# }
