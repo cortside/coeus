53e83590798ee6f50d0125cdf7fb8b48f93a88d2
Ruifang Li <ruifang.li09@gmail.com>
2023-05-23 20:21:31 -0600

    intermediate catalog files

---
+++ shoppingcart-web/src/app/catalog/catalog/catalog.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-catalog',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './catalog.component.html',
+  styleUrls: ['./catalog.component.scss']
+})
+export class CatalogComponent {
+
+}
