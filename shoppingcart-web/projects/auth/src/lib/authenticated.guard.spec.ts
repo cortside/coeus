5d733647ef8a914a0f47e040da7b8b5b2470e013
ruifang <ruifang.li09@gmail.com>
2023-08-02 21:02:06 -0600

    rename file

--- shoppingcart-web/projects/auth/src/lib/authenticated.guard.spec.ts
+++ shoppingcart-web/projects/auth/src/lib/authenticated.guard.spec.ts
@@ -1,6 +1,6 @@
 import { TestBed } from '@angular/core/testing';
 
-import { AuthenticatedGuard } from './authentication.guard';
+import { AuthenticatedGuard } from './authenticated.guard';
 
 describe('AuthenticatedGuard', () => {
   let guard: AuthenticatedGuard;
