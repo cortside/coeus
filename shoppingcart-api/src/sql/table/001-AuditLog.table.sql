62a142cb77647d465a69b339ad3c5a17ab4b36e9
bvieregge-enerbank <142243706+bvieregge-enerbank@users.noreply.github.com>
2023-08-22 16:00:51 -0500

    Moving migration scripts to audit table scripts

--- shoppingcart-api/src/sql/table/001-AuditLog.table.sql
+++ shoppingcart-api/src/sql/table/001-AuditLog.table.sql
@@ -58,3 +58,60 @@ GO
 
 drop procedure #spAlterColumn_AuditLog
 GO
+
+IF NOT EXISTS (SELECT COLUMN_NAME FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = 'AuditLog' AND TABLE_SCHEMA = 'audit' AND COLUMN_NAME = 'AuditLogId' AND DATA_TYPE = 'bigint')
+BEGIN
+	IF (SELECT OBJECTPROPERTY(OBJECT_ID(N'Audit.AuditLogTransaction'),'TableHasPrimaryKey')) = 1
+	BEGIN
+		ALTER TABLE [audit].[AuditLog] DROP CONSTRAINT FK_AUDIT_LOG_TRANSACTION_ID
+		DROP INDEX [audit].[AuditLog].[IDX_AuditLog_AuditLogTransactionId]
+		DECLARE @PrimaryKeyName VARCHAR(50)
+		SELECT @PrimaryKeyName = CONSTRAINT_NAME 
+		FROM   INFORMATION_SCHEMA.TABLE_CONSTRAINTS
+		WHERE  TABLE_NAME = 'AuditLogTransaction'
+		   AND TABLE_SCHEMA = 'audit'
+		   AND CONSTRAINT_TYPE = 'PRIMARY KEY' 
+		DECLARE @Command VARCHAR(500)
+		SELECT @Command = 'ALTER TABLE [Audit].[AuditLogTransaction] DROP CONSTRAINT ' + @PrimaryKeyName
+		EXECUTE (@Command)
+	END
+    ALTER TABLE [Audit].[AuditLogTransaction] ALTER COLUMN AuditLogTransactionId BIGINT
+	ALTER TABLE [Audit].[AuditLog] ALTER COLUMN AuditLogTransactionId BIGINT
+	
+	IF (SELECT OBJECTPROPERTY(OBJECT_ID(N'Audit.AuditLogTransaction'),'TableHasPrimaryKey')) = 0
+	BEGIN
+		ALTER TABLE [audit].[AuditLogTransaction] ADD PRIMARY KEY CLUSTERED 
+		(
+			[AuditLogTransactionId] ASC
+		)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ONLINE = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+		
+		ALTER TABLE [audit].[AuditLog]  WITH CHECK ADD  CONSTRAINT [FK_AUDIT_LOG_TRANSACTION_ID] FOREIGN KEY([AuditLogTransactionId])
+		REFERENCES [audit].[AuditLogTransaction] ([AuditLogTransactionId])
+		CREATE NONCLUSTERED INDEX [IDX_AuditLog_AuditLogTransactionId] ON [audit].[AuditLog]
+		(
+			[AuditLogTransactionId] ASC
+		)
+	END
+
+	IF (SELECT OBJECTPROPERTY(OBJECT_ID(N'Audit.AuditLog'),'TableHasPrimaryKey')) = 1
+	BEGIN
+		DECLARE @PrimaryKeyName VARCHAR(50)
+		SELECT @PrimaryKeyName = CONSTRAINT_NAME 
+		FROM   INFORMATION_SCHEMA.TABLE_CONSTRAINTS
+		WHERE  TABLE_NAME = 'AuditLog'
+		   AND TABLE_SCHEMA = 'audit'
+		   AND CONSTRAINT_TYPE = 'PRIMARY KEY' 
+		DECLARE @Command VARCHAR(500)
+		SELECT @Command = 'ALTER TABLE [Audit].[AuditLog] DROP CONSTRAINT ' + @PrimaryKeyName
+		EXECUTE (@Command)
+	END
+    ALTER TABLE [Audit].[AuditLog] ALTER COLUMN AuditLogId BIGINT
+	
+	IF (SELECT OBJECTPROPERTY(OBJECT_ID(N'Audit.AuditLog'),'TableHasPrimaryKey')) = 0
+	BEGIN
+		ALTER TABLE [audit].[AuditLog] ADD PRIMARY KEY CLUSTERED 
+		(
+			[AuditLogId] ASC
+		)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ONLINE = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+	END
+END
