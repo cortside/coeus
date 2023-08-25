71eaa245a68e4c1fb5ef77e251fd2cc25f3f2d89
ruifang <ruifang.li09@gmail.com>
2023-08-25 13:58:17 -0600

    remove file not in use

--- shoppingcart-web/projects/auth/src/public-api.ts
+++ shoppingcart-web/projects/auth/src/public-api.ts
@@ -7,7 +7,6 @@ export * from './lib/authentication.service';
 export * from './lib/authenticated-user';
 export * from './lib/authorization.service';
 export * from './lib/authentication-token.interceptor';
-export * from './lib/authentication-initializer.provider';
 export * from './lib/authenticated.guard';
 export * from './lib/authorization-policy';
 export * from './lib/authorization-data';
