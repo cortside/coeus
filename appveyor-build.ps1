01c71addc06b0001f329a32c875cad812559edfe
Cort Schaefer <cort@eschaefer.net>
2023-08-30 15:31:59 -0600

    [SUG-10] use pr source and target branches to identify changed files

--- appveyor-build.ps1
+++ appveyor-build.ps1
@@ -28,7 +28,7 @@ Write-Host Starting build
 $files = ""
 if ( $env:APPVEYOR_PULL_REQUEST_NUMBER ) {
   Write-Host Pull request $env:APPVEYOR_PULL_REQUEST_NUMBER
-  $files = $(git --no-pager diff --name-only FETCH_HEAD $(git merge-base FETCH_HEAD master))
+  $files = $(git --no-pager diff --name-only $target..$branch)
 } else {
   Write-Host Branch $env:APPVEYOR_REPO_BRANCH
   $files = $(git diff --name-only HEAD~1)
