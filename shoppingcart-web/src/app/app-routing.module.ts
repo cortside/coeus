f2bf928f95a694150ed66300d570c5f72eaf624d
ruifang <ruifang.li09@gmail.com>
2023-09-12 16:06:48 -0600

    add style guide component

--- shoppingcart-web/src/app/app-routing.module.ts
+++ shoppingcart-web/src/app/app-routing.module.ts
@@ -1,13 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { StyleGuideComponent } from 'muzieh-design';
 import { catalogLazyRoutes } from './catalog/catalog.routes';
 import { checkoutLazyLoadingRoutes } from './checkout/checkout-routing.module';
 import { orderLazyLoadingRoutes } from './order';
 
-const routes: Routes = [{ path: '', redirectTo: '/catalog/items', pathMatch: 'full' }, { path: 'logout', redirectTo: '/', pathMatch: 'full' }, ...orderLazyLoadingRoutes, ...checkoutLazyLoadingRoutes, ...catalogLazyRoutes];
+const routes: Routes = [{ path: '', redirectTo: '/catalog/items', pathMatch: 'full' }, { path: 'logout', redirectTo: '/', pathMatch: 'full' }, { path: 'styleguide', component: StyleGuideComponent }, ...orderLazyLoadingRoutes, ...checkoutLazyLoadingRoutes, ...catalogLazyRoutes];
 
 @NgModule({
-    imports: [RouterModule.forRoot(routes)],
+    imports: [RouterModule.forRoot(routes), StyleGuideComponent],
     exports: [RouterModule],
 })
 export class AppRoutingModule {}
