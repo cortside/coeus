7ef14f9156fe4db5c28cc66ffb7f16f0c4a3f7fa
ruifang <ruifang.li09@gmail.com>
2023-12-01 15:26:33 -0700

    add page not found component

--- shoppingcart-web/src/app/app-routing.module.ts
+++ shoppingcart-web/src/app/app-routing.module.ts
@@ -3,8 +3,16 @@ import { RouterModule, Routes } from '@angular/router';
 import { catalogLazyRoutes } from './catalog/catalog.routes';
 import { checkoutLazyLoadingRoutes } from './checkout/checkout-routing.module';
 import { orderLazyLoadingRoutes } from './order';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
-const routes: Routes = [{ path: '', redirectTo: '/catalog/items', pathMatch: 'full' }, { path: 'logout', redirectTo: '/', pathMatch: 'full' }, ...orderLazyLoadingRoutes, ...checkoutLazyLoadingRoutes, ...catalogLazyRoutes];
+const routes: Routes = [
+        { path: '', redirectTo: '/catalog/items', pathMatch: 'full' }, 
+        { path: 'logout', redirectTo: '/', pathMatch: 'full' }, 
+        ...orderLazyLoadingRoutes, 
+        ...checkoutLazyLoadingRoutes, 
+        ...catalogLazyRoutes,
+        { path: '**', component: PageNotFoundComponent }
+    ];
 
 @NgModule({
     imports: [RouterModule.forRoot(routes)],
