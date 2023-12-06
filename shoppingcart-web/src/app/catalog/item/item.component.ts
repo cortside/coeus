62653834fad2ac99f39cc7d8fd09f30dd2281287
ruifang <ruifang.li09@gmail.com>
2023-12-06 13:04:53 -0700

    render item details

--- shoppingcart-web/src/app/catalog/item/item.component.ts
+++ shoppingcart-web/src/app/catalog/item/item.component.ts
@@ -1,5 +1,10 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ItemService } from 'src/app/core/item.service';
+import { ItemModel } from '../models/item.model';
+import { ActivatedRoute } from '@angular/router';
+import { toSignal } from '@angular/core/rxjs-interop';
+import { Signal } from '@angular/core';
 
 @Component({
     selector: 'app-item',
@@ -8,4 +13,10 @@ import { CommonModule } from '@angular/common';
     templateUrl: './item.component.html',
     styleUrls: ['./item.component.scss'],
 })
-export class ItemComponent {}
+export class ItemComponent {
+    
+    item: Signal<ItemModel | undefined>;
+    constructor(private route: ActivatedRoute, private service: ItemService){
+        this.item = toSignal(service.getItem(route.snapshot.params['sku']));
+    }
+}
