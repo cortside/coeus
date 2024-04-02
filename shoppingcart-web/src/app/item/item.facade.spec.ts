ad80afbe9864d4f2ebf5c02c992d4a2c35a9ad51
ruifang <ruifang.li09@gmail.com>
2024-04-02 13:57:32 -0600

    generate facade files

---
+++ shoppingcart-web/src/app/item/item.facade.spec.ts
@@ -0,0 +1,7 @@
+import { ItemFacade } from './item-facade.facade';
+
+describe('ItemFacade', () => {
+  it('should create an instance', () => {
+    expect(new ItemFacade()).toBeTruthy();
+  });
+});
