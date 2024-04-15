cd1dcdf44e50fc0a02a34302e6717bde42c54094
ruifang <ruifang.li09@gmail.com>
2024-04-15 15:18:33 -0600

    add lint rule

--- shoppingcart-web/src/app/core/global-error-handler.ts
+++ shoppingcart-web/src/app/core/global-error-handler.ts
@@ -4,6 +4,8 @@ import { Logger, LOGGER } from "@muziehdesign/core";
 @Injectable()
 export class GlobalErrorHandler implements ErrorHandler {
     constructor(@Inject (LOGGER) private logger: Logger) {}
+
+    /* eslint-disable  @typescript-eslint/no-explicit-any */
     handleError(error: any): void {
         this.logger.error(error); // TODO: log more things
     }
