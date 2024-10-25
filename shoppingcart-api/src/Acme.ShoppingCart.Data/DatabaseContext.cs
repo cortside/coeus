b4a33b5f267a697907a545fc69a04cf4a8c1d607
Cort Schaefer <cort@eschaefer.net>
2024-10-24 19:15:54 -0600

    update to latest packages; update DatabaseContext to use extention methods moved to ModelBuilder and add example of custom interceptor

--- shoppingcart-api/src/Acme.ShoppingCart.Data/DatabaseContext.cs
+++ shoppingcart-api/src/Acme.ShoppingCart.Data/DatabaseContext.cs
@@ -1,9 +1,8 @@
-using System;
-using System.Linq;
-using System.Threading.Tasks;
+using Acme.ShoppingCart.Data.Interceptors;
 using Acme.ShoppingCart.Domain.Entities;
 using Cortside.AspNetCore.Auditable;
 using Cortside.AspNetCore.Auditable.Entities;
+using Cortside.AspNetCore.Common;
 using Cortside.AspNetCore.EntityFramework;
 using Cortside.Common.Security;
 using Cortside.DomainEvent.EntityFramework;
@@ -12,6 +11,8 @@ using Microsoft.EntityFrameworkCore;
 namespace Acme.ShoppingCart.Data {
     public class DatabaseContext : UnitOfWorkContext<Subject>, IDatabaseContext {
         public DatabaseContext(DbContextOptions options, ISubjectPrincipal subjectPrincipal, ISubjectFactory<Subject> subjectFactory) : base(options, subjectPrincipal, subjectFactory) {
+            // example of how to set an InternalDateTimeHandling other than the default (which is Utc)
+            DateTimeHandling = InternalDateTimeHandling.Utc;
         }
 
         public DbSet<Customer> Customers { get; set; }
@@ -20,19 +21,17 @@ namespace Acme.ShoppingCart.Data {
         protected override void OnModelCreating(ModelBuilder modelBuilder) {
             modelBuilder.HasDefaultSchema("dbo");
             modelBuilder.AddDomainEventOutbox();
-
-            // TOTO: make modelBuilder extensions
-            SetDateTime(modelBuilder);
-            SetCascadeDelete(modelBuilder);
+            modelBuilder.SetDateTime();
+            modelBuilder.SetCascadeDelete();
         }
 
         /// <summary>
-        /// Hook to add additional logic before entities are actually saved.  Shown here for example, only override if there is actual need.
+        /// Example of how to set a custom interceptor, method only needed if an interceptor is added
         /// </summary>
-        /// <param name="updatingSubject"></param>
-        /// <returns></returns>
-        protected override Task OnBeforeSaveChangesAsync(Subject updatingSubject) {
-            return Console.Out.WriteLineAsync($"Change tracker has {ChangeTracker.Entries().Count()} entries");
+        /// <param name="optionsBuilder"></param>
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) {
+            optionsBuilder.AddInterceptors(new ExampleInterceptor<Subject>(Subjects, DateTimeHandling, subjectPrincipal, subjectFactory));
+            base.OnConfiguring(optionsBuilder);
         }
     }
 }
---
+++ shoppingcart-api/src/Acme.ShoppingCart.Data/Interceptors/ExampleInterceptor.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Cortside.AspNetCore.Auditable;
+using Cortside.AspNetCore.Auditable.Entities;
+using Cortside.AspNetCore.Common;
+using Cortside.AspNetCore.EntityFramework.Interceptors;
+using Cortside.Common.Security;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+
+namespace Acme.ShoppingCart.Data.Interceptors {
+    /// <summary>
+    /// Example to add additional logic before entities are actually saved.  Shown here for example, only implement if there is actual need.
+    /// </summary>
+    public class ExampleInterceptor<TSubject> : AuditableSaveChangesInterceptor<TSubject> where TSubject : Subject {
+
+        public ExampleInterceptor(DbSet<TSubject> subjects, InternalDateTimeHandling dateTimeHandling, ISubjectPrincipal subjectPrincipal, ISubjectFactory<TSubject> subjectFactory) : base(subjects, dateTimeHandling, subjectPrincipal, subjectFactory) {
+        }
+
+        public override async ValueTask<InterceptionResult<int>> SavingChangesAsync(DbContextEventData eventData, InterceptionResult<int> result, CancellationToken cancellationToken = default) {
+            // check for subject in subjects set and either create or get to attach to AudibleEntity
+            var updatingSubject = await GetCurrentSubjectAsync();
+
+            DbContext dbContext = eventData.Context;
+            if (dbContext is not null) {
+                var entries = dbContext.ChangeTracker.Entries().Count();
+                var s = $"Change tracker has {entries} entries by user {updatingSubject.UserPrincipalName}";
+                await Console.Out.WriteLineAsync(new StringBuilder(s), cancellationToken);
+            }
+
+            return await base.SavingChangesAsync(eventData, result, cancellationToken);
+        }
+    }
+}
