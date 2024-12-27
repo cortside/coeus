a3d44c18ecf2d2d92ee3809f0b2fbbc1d7feb2c2
Cort Schaefer <cort@eschaefer.net>
2024-12-27 12:54:45 -0700

    remove unneeded output

--- shoppingcart-api/format.ps1
+++ shoppingcart-api/format.ps1
@@ -9,10 +9,6 @@ Param(
 	[string[]]$filePaths
 )
 
-	Write-Output "===== USAGE EXAMPLE ====="
-	Write-Output  'PS C:\work\Acme.ShoppingCart> .\convert-encoding.ps1 -filePaths ((gci *.trigger.sql -Recurse) | % { $_.FullName })'
-	Write-Output "========================="
-
 	foreach ($file in $filePaths) {
 		Write-Output "Convert encoding to UTF-8 for $file"
 		$MyFile = Get-Content $file
