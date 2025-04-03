c9ef17ecd3a2c6f148ea9bdc72708feb47d89c3c
Ruifang Li <ruifang.li09@gmail.com>
2025-04-02 18:55:27 -0600

    upgrade to angular 18

--- shoppingcart-web/src/app/api/catalog/catalog.client.spec.ts
+++ shoppingcart-web/src/app/api/catalog/catalog.client.spec.ts
@@ -1,16 +1,17 @@
 import { TestBed } from '@angular/core/testing';
-import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { provideHttpClientTesting } from '@angular/common/http/testing';
 import { CatalogClient } from './catalog.client';
 import { AppConfig } from 'src/environments/app-config';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 
 describe('CatalogClient', () => {
     let service: CatalogClient;
 
     beforeEach(() => {
         TestBed.configureTestingModule({
-            imports: [HttpClientTestingModule],
-            providers: [{ provide: AppConfig, useValue: {} }],
-        });
+    imports: [],
+    providers: [{ provide: AppConfig, useValue: {} }, provideHttpClient(withInterceptorsFromDi()), provideHttpClientTesting()]
+});
         service = TestBed.inject(CatalogClient);
     });
 
