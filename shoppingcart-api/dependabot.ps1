2ead8635309b6454cdb60720134007f63f396791
Matt A. Wilkinson <mwilkinson@enerbankusa.com>
2025-08-18 12:23:35 -0500

    reverse switch

--- shoppingcart-api/dependabot.ps1
+++ shoppingcart-api/dependabot.ps1
@@ -1,26 +1,26 @@
 [CmdletBinding()]
 Param 
 (
-    [Parameter(Mandatory = $false)][string]$package = "",
- [Parameter(Mandatory = $false)][string]$preupdateExpression = "",
- [Parameter(Mandatory = $false)][switch]$createPullRequest,
- [Parameter(Mandatory = $false)][switch]$ignoreChanges,
- [Parameter(Mandatory = $false)][string]$ticketId = "",
- [Parameter(Mandatory = $false)][switch]$runTests
+	[Parameter(Mandatory = $false)][string]$package = "",
+	[Parameter(Mandatory = $false)][string]$preupdateExpression = "",
+	[Parameter(Mandatory = $false)][switch]$createPullRequest,
+	[Parameter(Mandatory = $false)][switch]$ignoreChanges,
+	[Parameter(Mandatory = $false)][string]$ticketId = "",
+	[Parameter(Mandatory = $false)][switch]$skipTests
 )
 
 $ErrorActionPreference = "Stop"
 
 Function Invoke-BuildError {
 Param(
- [parameter(Mandatory=$true)][string] $text
+	[parameter(Mandatory=$true)][string] $text
 )
- # cleanup and undo changes
- ./clean.ps1
- git checkout -- *
+	# cleanup and undo changes
+	./clean.ps1
+	git checkout -- *
  
- Write-Error $text 
- exit 1
+	Write-Error $text 
+	exit 1
 }
 
 Function check-result {
@@ -46,9 +46,9 @@ Function Invoke-Exe {
 # check for uncommitted changed files
 $changes = (git status --porcelain)
 if ($changes.Count -ne 0 -and -not $ignoreChanges.IsPresent) {
- Write-Output "Exiting, sandbox has $($changes.Count) changes"
- Write-Output $changes
- exit 1
+	Write-Output "Exiting, sandbox has $($changes.Count) changes"
+	Write-Output $changes
+	exit 1
 }
 
 # make sure this is done from current develop branch
@@ -59,15 +59,15 @@ Invoke-Exe git -args "pull"
 $bot = "BOT-{0:yyyyMMdd}" -f (Get-Date)
 $branch = "feature/$bot"
 if ($ticketId -ne "") {
- $branch = "feature/$ticketId"
+	$branch = "feature/$ticketId"
 } elseif ($package -ne "") {
- $branch = "feature/$bot-$package"
+	$branch = "feature/$bot-$package"
 }
 
 $exists = (git branch -r | sls $branch)
 if ($exists -ne $null) {
- Write-Output "Exiting, $branch already exists"
- exit 1
+	Write-Output "Exiting, $branch already exists"
+	exit 1
 }
 
 echo "prepping"
@@ -78,8 +78,8 @@ $result = check-result
 echo "preupdate"
 
 if ($preupdateExpression -ne "") {
- echo "running $preupdateExpression"
- Invoke-Expression "& $preupdateExpression"
+	echo "running $preupdateExpression"
+	Invoke-Expression "& $preupdateExpression"
 }
 
 echo "about to restore"
@@ -89,9 +89,9 @@ $result = check-result
 
 echo "ready to update nuget packages"
 if ($package -eq "") { 
- $body = (.\update-nugetpackages.ps1)
+	$body = (.\update-nugetpackages.ps1)
 } else {
- $body = (Invoke-Exe dotnet -args "outdated src --include $package --pre-release Never --upgrade")
+	$body = (Invoke-Exe dotnet -args "outdated src --include $package --pre-release Never --upgrade")
 }
 echo $body
 $result = check-result
@@ -100,9 +100,9 @@ echo "checking to see if anything changed"
 
 $changes = (git status --porcelain)
 if ($changes.Count -ne 0) {
- if ($runTests) {
- dotnet test src
- $result = check-result
+ if (-not $skipTests) {
+	dotnet test src
+	$result = check-result
  }
 
  git status
@@ -110,35 +110,35 @@ if ($changes.Count -ne 0) {
  $bot = "BOT-{0:yyyyMMdd}" -f (Get-Date)
  $branch = "feature/$bot"
  if ($ticketId -ne "") {
-  $branch = "feature/$ticketId"
+	$branch = "feature/$ticketId"
  } elseif ($package -ne "") {
-  $branch = "feature/$bot-$package"
+	$branch = "feature/$bot-$package"
  }
  
  git add -u -u
  git status
  git checkout -b $branch
  if ($ticketId -ne "") {
-  git commit -m "[$ticketId] updated nuget packages"
+	git commit -m "[$ticketId] updated nuget packages"
  } elseif ($package -eq "") {
-  git commit -m "[$branch] updated nuget packages"
+	git commit -m "[$branch] updated nuget packages"
  } else {
-  git commit -m "[$branch] update $package"
+	git commit -m "[$branch] update $package"
  }
  git push --set-upstream origin $branch
 
  $remote = (git remote -v)
  $ghexists = if (Get-Command "gh.exe" -ErrorAction SilentlyContinue) { $true } else { $false }
  if ($remote -like "*github.com*" -and $ghexists) {
-  gh repo set-default
-  gh pr create --title "$bot" --body "$body" --base develop
+	gh repo set-default
+	gh pr create --title "$bot" --body "$body" --base develop
  } else {
-  echo "should create the pr here -- everything passed - $branch" 
-  echo $body 
+	echo "should create the pr here -- everything passed - $branch" 
+	echo $body 
  }
 
  .\clean.ps1
  git checkout develop
 } else {
- echo "no files changed"
+	echo "no files changed"
 }
