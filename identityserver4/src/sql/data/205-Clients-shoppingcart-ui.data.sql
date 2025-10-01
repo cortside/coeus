df7ca7e2e2e30a44d40ee8e383b9d63d41783068
ruifang <ruifang.li09@gmail.com>
2025-10-01 10:00:57 -0600

    add new client

---
+++ identityserver4/src/sql/data/205-Clients-shoppingcart-ui.data.sql
@@ -0,0 +1,87 @@
+declare @clientId nvarchar(400)
+set @clientId='shoppingcart-ui'
+declare @id int
+
+if (not exists(select * from auth.Clients where clientId=@clientId))
+  BEGIN
+	INSERT [AUTH].[Clients] ([AbsoluteRefreshTokenLifetime], [AccessTokenLifetime], [AccessTokenType], [AllowAccessTokensViaBrowser], [AllowOfflineAccess], [AllowPlainTextPkce], [AllowRememberConsent], [AlwaysIncludeUserClaimsInIdToken], [AlwaysSendClientClaims], [AuthorizationCodeLifetime], [BackChannelLogoutSessionRequired], [BackChannelLogoutUri], [ClientClaimsPrefix], [ClientId], [ClientName], [ClientUri], [ConsentLifetime], [Description], [EnableLocalLogin], [Enabled], [FrontChannelLogoutSessionRequired], [FrontChannelLogoutUri], [IdentityTokenLifetime], [IncludeJwtId], [LogoUri], [PairWiseSubjectSalt], [LogoutSessionRequired], [LogoutUri], [NonEditable], [ProtocolType], [RefreshTokenExpiration], [RefreshTokenUsage], [RequireClientSecret], [RequireConsent], [RequirePkce], [SlidingRefreshTokenLifetime], [UpdateAccessTokenClaimsOnRefresh]) 
+		VALUES (2592000, 3600, 0, 1, 0, 0, 1, 1, 1, 300, 1, NULL, N'client_', @clientId, N'ShoppingCart Ui', NULL, NULL, NULL, 1, 1, 1, NULL, 300, 0, NULL, NULL, 0, NULL, 0, N'oidc', 1, 1, 1, 0, 0, 1296000, 0)
+
+	set @id = SCOPE_IDENTITY()
+
+	--INSERT [AUTH].[ClientClaims] ([ClientId], [Type], [Value]) VALUES (@id, N'role', N'admin')
+	INSERT [AUTH].[ClientGrantTypes] ([ClientId], [GrantType]) VALUES (@id, N'authorization_code')
+  END
+
+select @id=id from auth.clients where clientId=@clientId
+
+-- Client settings
+update [AUTH].[Clients] set RequireConsent = 0, AllowRememberConsent = 0,
+	RequireClientSecret = 0,
+	EnableLocalLogin = 1,
+    RefreshTokenExpiration = 0, -- TokenExpiration.Sliding
+	AllowAccessTokensViaBrowser = 0,
+    RefreshTokenUsage = 1, -- TokenUsage.OneTimeOnly
+	RequirePkce = 1,
+    AllowPlainTextPkce=0,
+    AllowOfflineAccess = 1,
+	AccessTokenType = 1, -- AccessTokenType.Reference
+	AccessTokenLifetime = 120
+where Id = @id
+
+  --setup desired scopes
+print N'setup desired scopes'
+declare @grantedScopes table (
+    Scope nvarchar(200)
+)
+insert into @grantedScopes values (N'openid')
+insert into @grantedScopes values (N'profile')
+insert into @grantedScopes values (N'role')
+insert into @grantedScopes values (N'offline_access')
+insert into @grantedScopes values (N'shoppingcart-api')
+insert into @grantedScopes values (N'catalog-api')
+
+insert into [AUTH].[ClientScopes] (ClientId, Scope)
+select @id, a.Scope
+from @grantedScopes a
+where Scope not in (select Scope from auth.ClientScopes where clientId=@id)
+
+
+declare @uris table (
+    Uri nvarchar(200)
+)
+insert into @uris values (N'http://localhost:4200')
+insert into @uris values (N'http://kehlstein:5003')
+insert into @uris values (N'http://kehlstein:5004')
+insert into @uris values (N'https://shoppingcart.cortside.net')
+INSERT [AUTH].[ClientCorsOrigins] ([ClientId], [Origin])
+select @id, a.Uri
+from @uris a
+where uri not in (select origin from auth.ClientCorsOrigins where clientId=@id)
+
+
+DELETE FROM @uris
+insert into @uris values (N'http://localhost:4200/logout')
+insert into @uris values (N'http://kehlstein:5003/logout')
+insert into @uris values (N'http://kehlstein:5004/logout')
+insert into @uris values (N'https://shoppingcart.cortside.net/logout')
+INSERT [AUTH].[ClientPostLogoutRedirectUris] ([ClientId], [PostLogoutRedirectUri])
+select @id, a.Uri
+from @uris a
+where uri not in (select PostLogoutRedirectUri from auth.ClientPostLogoutRedirectUris where clientId=@id)
+
+-- setup allowed redirect uris
+print N'setup allowed redirect uris'
+declare @allowedRedirectUris table (
+    Uri nvarchar(1000)
+)
+insert into @allowedRedirectUris values (N'http://localhost:4200/login-redirect')
+insert into @allowedRedirectUris values (N'http://localhost:4200/oidc/silent-redirect.html')
+insert into @allowedRedirectUris values (N'http://kehlstein:5004/login-redirect')
+insert into @allowedRedirectUris values (N'http://kehlstein:5004/oidc/silent-redirect.html')
+insert into @allowedRedirectUris values (N'https://shoppingcart.cortside.net/login-redirect')
+insert into @allowedRedirectUris values (N'https://shoppingcart.cortside.net/oidc/silent-redirect.html')
+insert into [AUTH].[ClientRedirectUris] (ClientId, RedirectUri)
+select @id, a.Uri
+from @allowedRedirectUris a
+where uri not in (select RedirectUri from auth.ClientRedirectUris where clientId=@id)
